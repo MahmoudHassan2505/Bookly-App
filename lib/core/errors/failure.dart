4a5eba17e0982c6b068b4606ce3b102b1a706263
Mahmoud Hassan <mshmoudghania@gmail.com>
2024-06-03 02:25:12 +0300

    Failure class and repo abstract method

---
+++ lib/core/errors/failure.dart
@@ -0,0 +1,5 @@
+//this class include all failure
+//create a class for each error and extend the abstract class
+abstract class Failure {}
+
+class ServerFailure extends Failure {}
--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,20 @@
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/features/home/data/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchAllBooks();
-  fetchFavouriteBooks();
+  //this method will take time to fetch data so it will be future return type
+  //there colud be an error when fetching data so the return type may be null
+  //so  if request succes it will return book model if failed it will return something else (retrun  two data TYPE !!!!!!!!!)
+  // there is a package called dartz
+//------------------------------------------------------------
+  // Future<List<BookModel>> fetchAllBooks();
+  // Future<List<BookModel>> fetchFavouriteBooks();
+//--------------------------------------------------------
+//   Future<Either<L,R>> fetchAllBooks();
+//   L --> data type at Failure
+//   R --> data type at Success
+//----------------------------------------------
+  Future<Either<Failure, List<BookModel>>> fetchAllBooks();
+  Future<List<BookModel>> fetchFavouriteBooks();
 }
