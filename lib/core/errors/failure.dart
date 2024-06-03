7e66a656fbfa51c930520f9ff04ba195f53696fd
Mahmoud Hassan <mshmoudghania@gmail.com>
2024-06-03 03:35:03 +0300

    Implementing Dio Exception Handler

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,5 +1,38 @@
 //this class include all failure
 //create a class for each error and extend the abstract class
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errMesasge;
+  const Failure(this.errMesasge);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMesasge);
+
+  factory ServerFailure.fromDioException(DioException dio) {
+    switch (dio.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection Timeout  with API Server');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send Timeout  with API Server');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Recive Timeout  with API Server');
+      case DioExceptionType.badResponse:
+        return ServerFailure(dio.response!.data['error']['message']);
+      case DioExceptionType.cancel:
+        return ServerFailure('Connection Canceled  with API Server');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('badCertificate');
+      case DioExceptionType.connectionError:
+        return ServerFailure('Connection Error  with API Server');
+      case DioExceptionType.unknown:
+        if (dio.message!.contains('SocketException')) {
+          return ServerFailure('No Internet Connection');
+        }
+        return ServerFailure('Unkown Error, Try again Later');
+      default:
+        return ServerFailure("Unkown");
+    }
+  }
+}
